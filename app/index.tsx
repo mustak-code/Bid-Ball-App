33dd67dc6e003991801440139055efca35346012
iam-MustakAhmedKhan <mustakahmedkhan32@gmail.com>
2024-12-02 18:15:52 +0600

    tailwind-css initialized

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,11 @@
 import { Text, View } from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    return (
+        <View className="flex-1  items-center">
+            <Text className="text-rose-800 font-bold">
+                Edit app/index.tsx to edit this screen.
+            </Text>
+        </View>
+    );
 }
